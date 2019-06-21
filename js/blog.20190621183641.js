46b04021f6c7364a424eec527ae23f8bc94373c4
Manik <mnk@Mk-3.local>
2019-06-21 18:37:13 +0530

    Updated Info

---
+++ js/blog.20190621183641.js
@@ -0,0 +1,11 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+var lwi=-1;function thresholdPassed(){var w=$(window).width();var p=false;var cw=0;if(w>=960){cw++;}if(w>=1200){cw++;}if(lwi!=cw){p=true;}lwi=cw;return p;}
+
+$(document).ready(function() {
+r=function(){if(thresholdPassed()){dpi=window.devicePixelRatio;if($(window).width()>=1200){$('.js-25').attr('src', (dpi>1) ? 'images/pasted-image-626.png' : 'images/pasted-image-313.png');}else if($(window).width()>=960){$('.js-25').attr('src', (dpi>1) ? 'images/pasted-image-500.png' : 'images/pasted-image-250.png');}else{$('.js-25').attr('src', (dpi>1) ? 'images/pasted-image-404.png' : 'images/pasted-image-202.png');}}};
+if(!window.HTMLPictureElement){$(window).resize(r);r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+
+});
