103136358cfa66b38386913b1b767d2f685528f7
Manik <mnk_400@icloud.com>
2020-04-26 20:16:22 -0400

    spacing on mobile

--- js/index.20200426201552.js
+++ js/index.20200426201552.js
@@ -8,10 +8,10 @@ $(document).ready(function() {
 r=function(){if(thresholdPassed()){dpi=window.devicePixelRatio;if($(window).width()>=960){$('.js').attr('src', (dpi>1) ? 'images/email-40.png' : 'images/email-20.png');
 $('.js-2').attr('src', (dpi>1) ? 'images/github-2-46.png' : 'images/github-2-23.png');
 $('.js-3').attr('src', (dpi>1) ? 'images/linkedin-46.png' : 'images/linkedin-23.png');
-$('.js-4').attr('src', (dpi>1) ? 'images/8956ca07-c5a6-45b8-84dc-1d8d4f5622e2-726-460.jpg' : 'images/8956ca07-c5a6-45b8-84dc-1d8d4f5622e2-726-230.jpg');}else{$('.js').attr('src', (dpi>1) ? 'images/email-34.png' : 'images/email-17.png');
+$('.js-4').attr('src', (dpi>1) ? 'images/8956ca07-c5a6-45b8-84dc-1d8d4f5622e2-726-460-1.jpg' : 'images/8956ca07-c5a6-45b8-84dc-1d8d4f5622e2-726-230-1.jpg');}else{$('.js').attr('src', (dpi>1) ? 'images/email-34.png' : 'images/email-17.png');
 $('.js-2').attr('src', (dpi>1) ? 'images/github-2-42.png' : 'images/github-2-21.png');
 $('.js-3').attr('src', (dpi>1) ? 'images/linkedin-38.png' : 'images/linkedin-19.png');
-$('.js-4').attr('src', (dpi>1) ? 'images/8956ca07-c5a6-45b8-84dc-1d8d4f5622e2-726-460.jpg' : 'images/8956ca07-c5a6-45b8-84dc-1d8d4f5622e2-726-230.jpg');}}};
+$('.js-4').attr('src', (dpi>1) ? 'images/8956ca07-c5a6-45b8-84dc-1d8d4f5622e2-726-546.jpg' : 'images/8956ca07-c5a6-45b8-84dc-1d8d4f5622e2-726-273.jpg');}}};
 if(!window.HTMLPictureElement){$(window).resize(r);r();}
 (function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
 
