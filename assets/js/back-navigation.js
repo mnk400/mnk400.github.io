7d1ad08eed9b287538c91770f3ed834d413c598d
Manik Kumar <mnk_400@icloud.com>
2025-03-09 14:05:19 -0700

    better back button

---
+++ assets/js/back-navigation.js
@@ -0,0 +1,37 @@
+/**
+ * Handles the back button navigation logic
+ * - If the previous page is not from the same domain, redirects to homepage
+ * - If the previous page is the same as current page, redirects to homepage
+ * - Otherwise, performs normal browser back navigation
+ */
+function handleBackNavigation() {
+  const currentUrl = window.location.href;
+  const currentDomain = window.location.hostname;
+  
+  const referrer = document.referrer;
+
+  if (!referrer) {
+    window.location.href = '/';
+    return;
+  }
+  
+  let referrerUrl;
+  try {
+    referrerUrl = new URL(referrer);
+  } catch (e) {
+    window.location.href = '/';
+    return;
+  }
+  
+  if (referrerUrl.hostname !== currentDomain) {
+    window.location.href = '/';
+    return;
+  }
+  
+  if (referrer === currentUrl) {
+    window.location.href = '/';
+    return;
+  }
+  
+  window.history.back();
+}
