38d5205f423c6ce5b839cb8a546387a36e7f0943
Manik Kumar <mnk_400@icloud.com>
2025-03-10 23:36:15 -0700

    header updates

--- assets/js/textfader.js
+++ assets/js/textfader.js
@@ -1,4 +1,3 @@
-// Setup text splitting
 $('.text-fader .text-content').each(function () {
     var textSplit = $(this).text().split('');
     var returnHTML = textSplit.map(function (char) {
@@ -14,34 +13,43 @@ var textFaderAnimationSpeed = 1000;
 // Initialize text fading
 function initTextFader() {
     var $textContents = $(".text-fader .text-content");
+    var currentIndex = parseInt(localStorage.getItem('textFaderIndex')) || 0;
+    var lastUpdateTime = parseInt(localStorage.getItem('textFaderLastUpdate')) || 0;
+    var now = Date.now();
+    var timeDiff = now - lastUpdateTime;
 
-    // Immediately show first text
-    $textContents.first().addClass("in");
+    if (lastUpdateTime > 0) {
+        var cyclesPassed = Math.floor(timeDiff / (textFaderDelay + textFaderAnimationSpeed));
+        currentIndex = (currentIndex + cyclesPassed) % $textContents.length;
+    }
 
-    function cycleTexts($current) {
+    $textContents.removeClass('in out');
 
-        var $next = $current.next(".text-fader .text-content");
-        if ($next.length === 0) {
-            $next = $textContents.first();
-        }
+    $textContents.eq(currentIndex).addClass('in');
 
-        $current.removeClass("in").addClass("out");
+    function cycleTexts($current, index) {
+        var nextIndex = (index + 1) % $textContents.length;
+        var $next = $textContents.eq(nextIndex);
+
+        $current.removeClass('in').addClass('out');
+
+        // Store current state
+        localStorage.setItem('textFaderIndex', nextIndex);
+        localStorage.setItem('textFaderLastUpdate', Date.now());
 
         setTimeout(function () {
-            $current.removeClass("out");
-            $next.addClass("in");
+            $current.removeClass('out');
+            $next.addClass('in');
 
             setTimeout(function () {
-                cycleTexts($next);
+                cycleTexts($next, nextIndex);
             }, textFaderDelay);
         }, textFaderAnimationSpeed);
     }
 
-    // Start cycling after first delay
     setTimeout(function () {
-        cycleTexts($textContents.first());
+        cycleTexts($textContents.eq(currentIndex), currentIndex);
     }, textFaderDelay);
 }
 
-// Initialize on document ready
 $(document).ready(initTextFader);
