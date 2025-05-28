0b2c5649b1a8725335e91c71eed2018fce246e7f
Manik Kumar <mnk_400@icloud.com>
2025-05-28 00:36:25 -0700

    animations for header mode switches

--- assets/js/back-navigation.js
+++ assets/js/back-navigation.js
@@ -38,9 +38,18 @@ function handleBackNavigation() {
 
 function toggleHeaderVisibility(showFull) {
   const header = document.querySelector('header');
+  const minimalHeader = document.getElementById('minimalHeader');
+  const fullHeader = document.getElementById('fullHeader');
+
   if (showFull) {
+    // Transition to full header
     header.classList.remove('minimal-mode');
+    minimalHeader.classList.remove('visible');
+    fullHeader.classList.remove('hidden');
   } else {
+    // Transition to minimal header
     header.classList.add('minimal-mode');
+    minimalHeader.classList.add('visible');
+    fullHeader.classList.add('hidden');
   }
 }
