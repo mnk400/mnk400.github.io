d5a8fde966658a5aa5e187d60b5168d0d2b75bc0
Manik Kumar <mnk_400@icloud.com>
2026-04-12 16:15:44 -0700

    Creating an espresso based theme, fixing a couple minor UI bugs

--- assets/js/core/theme-config.js
+++ assets/js/core/theme-config.js
@@ -10,6 +10,7 @@ window.__themeConfig = {
     ruby:     { meta: "#2a1a1d" },
     matcha:   { meta: "#2a2f2a" },
     butter:   { meta: "#F1D799" },
+    espresso: { meta: "#352315" },
   },
   get names() { return Object.keys(this.themes); },
 };
